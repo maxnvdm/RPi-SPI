5ba27a5aacc75b38b803ff0618273cbc3d2220a4
Max <maxnvdm@gmail.com>
2018-09-11 22:41:59 +0200

    Designing initial framework for program.

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ SPIMISO = 9
 SPIMOSI = 10
 SPICS = 8
 
-
-
 GPIO.setup(reset_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(freq_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(stop_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -36,8 +34,12 @@ def reset(reset_btn):
 
 def freq(freq_btn):
 	# changes the sampling rate between: 500ms, 1s, 2s
-
-	pass
+	switcher = {
+		0.5: lambda frequency : frequency + 0.5 ,
+		1 : lambda frequency : frequency + 1,
+		2: lambda frequency : frequency - 1.5,
+	}
+	switcher.get(frequency)
 
 def stop(stop_btn):
 	# Stops or starts the reading (sampling) of the sensors
@@ -51,25 +53,26 @@ def display(display_btn):
 
 # assign callback functions to falling edge detection on pins
 GPIO.add_event_detect(reset_btn, GPIO.FALLING, callback=reset, bouncetime=200)
-GPIO.add_event_detect(freq_btn, GPIO.FALLING, callback=reset, bouncetime=200)
-GPIO.add_event_detect(stop_btn, GPIO.FALLING, callback=reset, bouncetime=200)
-GPIO.add_event_detect(display_btn, GPIO.FALLING, callback=reset, bouncetime=200)
+GPIO.add_event_detect(freq_btn, GPIO.FALLING, callback=freq, bouncetime=200)
+GPIO.add_event_detect(stop_btn, GPIO.FALLING, callback=stop, bouncetime=200)
+GPIO.add_event_detect(display_btn, GPIO.FALLING, callback=display, bouncetime=200)
 
 # Main loop
 try:
 	# initialise variables
 	frequency = 0.5     # sample rate s
-	# global variable
-	values = [0] * 8
+	values = [0] * 8    # ADC reading
+	timer = time.ctime()[10:18]
 
 	while True:
 		for i in range(8):
 			values[i] = mcp.read_adc(i)
 		# delay for a sample rate frequency
 		time.sleep(frequency)
+		timer = time.ctime()[10:18]
+		print(timer)
 		print(values)
 
-	pass
 except KeyboardInterrupt:
 	GPIO.cleanup()
 
